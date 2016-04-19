7d42fc1e6c91870ddc1423688f02911bd5f39a4b
szakolczaimartin <szakolczaimartin@gmail.com>
2016-04-19 14:37:56 +0200

    Create the GUI

--- Week14_SanityArchive/SanityArchive/Form1.Designer.cs
+++ Week14_SanityArchive/SanityArchive/Form1.Designer.cs
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DriverLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.compressButton = new System.Windows.Forms.Button();
+            this.encryptionButton = new System.Windows.Forms.Button();
+            this.driverLabel2 = new System.Windows.Forms.Label();
+            this.driverCombobox2 = new System.Windows.Forms.ComboBox();
+            this.fileListBox2 = new System.Windows.Forms.ListBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pathTextBox1 = new System.Windows.Forms.TextBox();
+            this.pathTextBox2 = new System.Windows.Forms.TextBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DriverLabel
+            // 
+            this.DriverLabel.AutoSize = true;
+            this.DriverLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DriverLabel.Location = new System.Drawing.Point(30, 15);
+            this.DriverLabel.Name = "DriverLabel";
+            this.DriverLabel.Size = new System.Drawing.Size(44, 16);
+            this.DriverLabel.TabIndex = 0;
+            this.DriverLabel.Text = "Driver";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(95, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(250, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.Location = new System.Drawing.Point(95, 128);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.Size = new System.Drawing.Size(250, 251);
+            this.fileListBox.TabIndex = 2;
+            // 
+            // compressButton
+            // 
+            this.compressButton.Location = new System.Drawing.Point(375, 344);
+            this.compressButton.Name = "compressButton";
+            this.compressButton.Size = new System.Drawing.Size(94, 35);
+            this.compressButton.TabIndex = 3;
+            this.compressButton.Text = "Compress";
+            this.compressButton.UseVisualStyleBackColor = true;
+            // 
+            // encryptionButton
+            // 
+            this.encryptionButton.Location = new System.Drawing.Point(375, 286);
+            this.encryptionButton.Name = "encryptionButton";
+            this.encryptionButton.Size = new System.Drawing.Size(94, 37);
+            this.encryptionButton.TabIndex = 4;
+            this.encryptionButton.Text = "Encryption";
+            this.encryptionButton.UseVisualStyleBackColor = true;
+            // 
+            // driverLabel2
+            // 
+            this.driverLabel2.AutoSize = true;
+            this.driverLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.driverLabel2.Location = new System.Drawing.Point(427, 51);
+            this.driverLabel2.Name = "driverLabel2";
+            this.driverLabel2.Size = new System.Drawing.Size(44, 16);
+            this.driverLabel2.TabIndex = 7;
+            this.driverLabel2.Text = "Driver";
+            // 
+            // driverCombobox2
+            // 
+            this.driverCombobox2.FormattingEnabled = true;
+            this.driverCombobox2.Location = new System.Drawing.Point(498, 50);
+            this.driverCombobox2.Name = "driverCombobox2";
+            this.driverCombobox2.Size = new System.Drawing.Size(268, 21);
+            this.driverCombobox2.TabIndex = 9;
+            // 
+            // fileListBox2
+            // 
+            this.fileListBox2.FormattingEnabled = true;
+            this.fileListBox2.Location = new System.Drawing.Point(498, 128);
+            this.fileListBox2.Name = "fileListBox2";
+            this.fileListBox2.Size = new System.Drawing.Size(268, 251);
+            this.fileListBox2.TabIndex = 11;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(375, 234);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(94, 33);
+            this.copyButton.TabIndex = 14;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            // 
+            // moveButton
+            // 
+            this.moveButton.Location = new System.Drawing.Point(375, 180);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(94, 33);
+            this.moveButton.TabIndex = 15;
+            this.moveButton.Text = "Move";
+            this.moveButton.UseVisualStyleBackColor = true;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(375, 128);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(94, 33);
+            this.editButton.TabIndex = 16;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(30, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(427, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Path";
+            // 
+            // pathTextBox1
+            // 
+            this.pathTextBox1.Location = new System.Drawing.Point(95, 53);
+            this.pathTextBox1.Name = "pathTextBox1";
+            this.pathTextBox1.Size = new System.Drawing.Size(250, 20);
+            this.pathTextBox1.TabIndex = 19;
+            // 
+            // pathTextBox2
+            // 
+            this.pathTextBox2.Location = new System.Drawing.Point(498, 90);
+            this.pathTextBox2.Name = "pathTextBox2";
+            this.pathTextBox2.Size = new System.Drawing.Size(268, 20);
+            this.pathTextBox2.TabIndex = 20;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(95, 90);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(250, 20);
+            this.searchTextBox.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(30, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Search";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(799, 407);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.pathTextBox2);
+            this.Controls.Add(this.pathTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.fileListBox2);
+            this.Controls.Add(this.driverCombobox2);
+            this.Controls.Add(this.driverLabel2);
+            this.Controls.Add(this.encryptionButton);
+            this.Controls.Add(this.compressButton);
+            this.Controls.Add(this.fileListBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.DriverLabel);
+            this.Name = "Form1";
+            this.Text = "Sanity archive";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label DriverLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox fileListBox;
+        private System.Windows.Forms.Button compressButton;
+        private System.Windows.Forms.Button encryptionButton;
+        private System.Windows.Forms.Label driverLabel2;
+        private System.Windows.Forms.ComboBox driverCombobox2;
+        private System.Windows.Forms.ListBox fileListBox2;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pathTextBox1;
+        private System.Windows.Forms.TextBox pathTextBox2;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label label3;
     }
 }
 
