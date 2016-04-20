cadc5b0c233a20c72df7cd079df5a82e3419f21b
Viczmándi Gergely <viczmandi@gmail.com>
2016-04-20 12:21:12 +0200

    FileCopyAndMove class created, Copy methods: CopyFile and CopyDirectory added.

---
+++ Week14_SanityArchive/SanityArchive/FileCopyAndMove.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace SanityArchive
+{
+    public class FileCopyAndMove
+    {
+        public void CopyFile(string sourcePath, string targetPath)
+        {
+            string fileName = new FileInfo(sourcePath).Name;
+            string destFilePath = Path.Combine(targetPath, fileName);
+
+            File.Copy(sourcePath, destFilePath, true);
+        }
+
+        public void CopyDirectory(string sourcePath, string targetPath)
+        {
+            string fileName;
+            string destFilePath;
+
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+
+            if (Directory.Exists(sourcePath))
+            {
+                string[] files = Directory.GetFiles(sourcePath);
+
+                foreach (string s in files)
+                {
+                    fileName = Path.GetFileName(s);
+                    destFilePath = Path.Combine(targetPath, fileName);
+                    File.Copy(s, destFilePath, true);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Source path does not exist!");
+            }
+        }
+    }
+}
